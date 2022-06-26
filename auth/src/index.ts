0bc607bb0f2e60a29cd78038bd18b57e6894041e
Muaz <muazwazir@gmail.com>
2022-06-27 00:13:03 +0800

    skaffold config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,8 @@ import { json } from "body-parser";
 
 const app = express();
 app.use(json());
+
 app.listen(3000, () => {
   console.log("Listening to port 3000");
+  console.log("Are you actually listening?");
 });
