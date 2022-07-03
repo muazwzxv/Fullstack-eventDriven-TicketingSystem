253b354dc3340f3501153e075aaad8092a84fb9a
Muaz <muazwazir@gmail.com>
2022-07-03 14:29:22 +0800

    added express-async-errors package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { registerRouter } from "./routes/register";
 import { loginRouter } from "./routes/login";
 import { logoutRouter } from "./routes/logout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -15,10 +16,14 @@ app.use(registerRouter);
 app.use(loginRouter);
 app.use(logoutRouter);
 
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 // register error handler
 app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening to port 3000");
-  console.log("Are you actually listening? for fuck sake");
+  console.log("Are you actually listening?");
 });
