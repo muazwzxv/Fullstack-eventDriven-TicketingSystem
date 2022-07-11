0b68760493c29834c7acc04990648811ec1119e6
Muaz <muazwazir@gmail.com>
2022-07-12 00:13:24 +0800

    wip

--- auth/src/routes/login.ts
+++ auth/src/routes/login.ts
@@ -1,7 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-router.get("", (req, res) => {});
+router.post(
+  "/api/users/login",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().isLength({ min: 4, max: 20 }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
+  }
+);
 
 export { router as loginRouter };
