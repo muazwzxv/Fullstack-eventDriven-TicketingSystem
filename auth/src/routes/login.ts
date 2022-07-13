09bdace63dba8689ae293bef2a43e64c574ab1f4
Muaz <muazwazir@gmail.com>
2022-07-14 00:52:26 +0800

    signin

--- auth/src/routes/login.ts
+++ auth/src/routes/login.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { User } from "../models/user";
 import { validateRequestMiddleware } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -11,7 +14,25 @@ router.post(
     body("password").trim().isLength({ min: 4, max: 20 }),
   ],
   validateRequestMiddleware,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const userFound = await User.findOne({ email });
+    if (!userFound) throw new BadRequestError("user does not exist");
+
+    const userJwt = jwt.sign(
+      {
+        id: userFound.id,
+        email: userFound.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200);
+  }
 );
 
 export { router as loginRouter };
