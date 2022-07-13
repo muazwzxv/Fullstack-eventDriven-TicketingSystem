8a966f974f82dd779519a006ac1631cbd4de5d04
Muaz <muazwazir@gmail.com>
2022-07-14 00:22:06 +0800

    middleware for validate request

--- auth/src/routes/login.ts
+++ auth/src/routes/login.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { body } from "express-validator";
+import { validateRequestMiddleware } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -10,11 +10,8 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password").trim().isLength({ min: 4, max: 20 }),
   ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
-  }
+  validateRequestMiddleware,
+  async (req: Request, res: Response) => {}
 );
 
 export { router as loginRouter };
--- auth/src/routes/register.ts
+++ auth/src/routes/register.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import { BadRequestError } from "../errors/bad-request";
-import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import jwt from "jsonwebtoken";
+import { validateRequestMiddleware } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -13,12 +13,8 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password").trim().isLength({ min: 4, max: 20 }),
   ],
+  validateRequestMiddleware,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
